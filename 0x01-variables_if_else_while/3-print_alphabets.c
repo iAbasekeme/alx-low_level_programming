fb314ec37d58dd7c736bbd338f2e4b79a2e93ea6
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-02-18 01:52:43 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,8 +10,8 @@ int main(void)
 
 	for (alp = 'a'; alp <= 'z'; alp++)
 		putchar(alp);
-	for (elp = 'A'; elp <= 'Z'; elp++)
-		putchar(elp);
+	for (alp = 'A'; alp <= 'Z'; alp++)
+		putchar(alp);
 		putchar('\n')'
 	return (0);
 }
