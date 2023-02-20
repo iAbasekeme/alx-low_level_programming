5b88c3f48e6b3b56cefaf95a5318d3aaf3b4ffb1
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-02-20 04:33:42 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,14 @@ int main(void)
 {
 	int ch, ch2;
 
-	for (ch = 0; ch < 9; ch++)
+	for (ch = '0'; ch < '9'; ch++)
 	{
-		for (ch2 = ch + 1; ch2 <= 9; ch2++)
+		for (ch2 = ch + '1'; ch2 <= '9'; ch2++)
 		{
 			putchar(ch);
 			putchar(ch2);
 
-			if (ch < 8 && ch2 <= 9)
-
+			if (ch < '8' && ch2 <= '9')
 			putchar(',');
 			putchar(' ');
 		}
