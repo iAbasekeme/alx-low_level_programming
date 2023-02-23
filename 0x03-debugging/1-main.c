fcffc88b14e08780df06f2291c068598bc8ceb65
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-02-23 11:16:28 +0100

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	printf("infinite loop incoming :("\n);
+	printf("infinite loop incoming :(\n");
 	
 	i = 0;
 	
@@ -17,7 +17,7 @@ int main(void)
 	/*	putchar(i);*/
 	/*}*/
 	
-	printf("infinite loop avoided! \\o/"\n);
+	printf("infinite loop avoided! \\o/\n");
 	
 	return (0);
 }
