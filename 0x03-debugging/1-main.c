6e15443c59816a7af48fe763511ecbb8a0b35a89
iAbasekeme <abasiekemeetim20@gmail.com>
2023-02-23 02:24:48 -0800

    Revert "added readme"
    This reverts commit e201bd0a8e927448f0a7a831b8109abfac4cba5f.

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,1 +1,23 @@
-1. Like, comment, subscribe
+#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+	
+	i = 0;
+	
+	/*while (i < 0)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+	
+	printf("infinite loop avoided! \\o/\n");
+	
+	return (0);
+}
