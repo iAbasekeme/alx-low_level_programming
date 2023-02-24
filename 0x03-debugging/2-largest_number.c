e9265504e3fe0b83cb563053f4feec58d69cae90
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-02-24 17:07:35 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else
+	else if (b > a && c > b)
 	{
 		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
 	}
 
 	return (largest);
