d677961eec725d46b3a352053fd55883c7f71d1f
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-02-25 00:30:03 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,8 +7,11 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
-		if (num != '2' && num != '4')
-			_putchar(num);
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+			_putchar((num % 10) + '0');
+	}
+
 	_putchar('\n');
 }
