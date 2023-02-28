475de6ef778a85282a5224043df74e4b3c9c4f92
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-02-28 11:55:48 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,12 +4,15 @@
  * _puts - a function that prints a string, followed by a new line, to stdout.
  * @str: string to be printed
  *
- * Return: (0)
+ * Return: (void)
  */
 
 void _puts(char *str)
 {
-	while(; *str != '\0'; str++)
+	while (; *str != '\0'; str++)
+	{
 		_putchar(*str);
+	}
+	
 	_putchar('\n');
 }
