fd7e888d075f361d10e26f7c2148f7b047968e1b
iAbasekeme <abasiekemeetim20@gmail.com>
2023-03-01 14:25:56 -0800

    fixed error

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,18 +10,19 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	int count = 0;
 	int len = 0;
 
 	while (dest[len])
 	{
 		len++;
 	}
+
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[len] = src[i];
 		len++;
 	}
+
 	dest[len] = '\0';
 	return (dest);
 }
