44de7201bb5ca7e693928adc4ee48ceb5b3a7b51
iAbasekeme <abasiekemeetim20@gmail.com>
2023-03-08 05:35:28 -0800

    Adds a program to print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - adds a program that prints a string in reverse.
+ * @s: string to be reversed
+ *
+ * Return: Nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
