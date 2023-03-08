1f14830bead3dc16b82acf296ddb63b3302d98e0
iAbasekeme <abasiekemeetim20@gmail.com>
2023-03-08 06:08:04 -0800

    calculate length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - claculate the length of a string
+ * @s: string to be calculated
+ *
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	return (_strlen_recursion(s + 1));
+}
