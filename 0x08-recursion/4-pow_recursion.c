255b461d2bac5a1875283387bd794a9e8b5426a4
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-03-10 01:50:53 +0100

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,11 +13,6 @@ int _pow_recursion(int x, int y)
 	while (y < 0)
 		return (-1);
 	if (y == 0)
-	{
 		return (1);
-	}
-	if (y > 0)
-	{
-		return (x * _pow_recursion(x, y - 1));
-	}
+	return (x * _pow_recursion(x, y - 1));
 }
