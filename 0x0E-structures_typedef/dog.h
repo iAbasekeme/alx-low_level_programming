c75628757e72ca7069796664d7610579c2d91f61
iAbasekeme <abasiekemeetim20@gmail.com>
2023-03-21 05:35:55 -0700

    fixed errors

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,18 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - an attribute of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of the owner
+ *
+ * Description: The attributes of a dog.
+ */
 struct dog
 {
-	char* name[];
+	char *name;
 	float age;
-	char* owner[];
+	char *owner;
 }
+#endif
