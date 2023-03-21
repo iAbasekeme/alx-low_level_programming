41d0a4a09561460a083a9df9ed968aa35cf6e707
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-03-21 15:04:35 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,8 +14,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		.name = name;
-		.age = age;
-		.owner = owner;
+		d.name = name;
+		d.age = age;
+		d.owner = owner;
 	};
 };
