4918e84746b3f59f23ecb87d53627e6b62e3be19
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-03-21 14:59:39 +0100

    Update main.h

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _HEADER_
+#define _HEADER_
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
