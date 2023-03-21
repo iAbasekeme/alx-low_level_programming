17547882af908e9af24ac584c1fc31bc9b9d0ff3
iAbasekeme <abasiekemeetim20@gmail.com>
2023-03-20 19:56:22 -0700

    fixed errors

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - expand into the current file
+ * @__FILE__ - the file to expand into
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
 	_putchar(__FIILE__);
