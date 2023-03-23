2c912afc25417338302e282443ea2de56b1ea371
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-03-23 14:13:11 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,12 @@
 
 /**
  * print_name - print a name
- *
  * @name: name to be printed
  * @f: pointer name
+ *
+ * Return: Nothing
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	f = *name;
+	f(name);
 }
