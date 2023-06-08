16d264647ea6a21eb18580c782fe8bf406c47fc8
iAbas <abasiekemeetim20@gmail.com>
2023-06-08 16:40:38 +0100

    Factorisation using recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - A function to find the factorial of a number
+ * @n: number to be factored
+ *
+ * Return: the factorial of the number
+ */
+int factorial(int n)
+{
+    int s = 0;
+
+    if (n < 0)
+        return -1;
+    else if (n == 1)
+        return n;
+    else
+    {
+        _putchar(n);
+        s = n * factorial(n - 1);
+    }
+    return s;
+}
