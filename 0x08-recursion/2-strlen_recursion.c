acd990e42abd0377d9ecde606c8bfdbb72b3dcd3
iAbas <abasiekemeetim20@gmail.com>
2023-06-08 11:44:25 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,15 +4,15 @@
  * _strlen_recursion - A function to determine the
  * length of a string using recursion
  *
- * @s - Recountable string
+ * @s: Recountable string
  *
  * Return: the length of the string
  */
 int _strlen_recursion(char *s)
 {
-int sum = 1;
+    int sum = 1;
 
-if (*s == '\0')
-return (0);
-return (_strlen_recursion(s + 1) + sum);
+    if (*s == '\0')
+        return (0);
+    return (_strlen_recursion(s + 1) + sum);
 }
