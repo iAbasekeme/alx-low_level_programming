1516b49723df0ea74e9a9b67e0038b61643fa1f3
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-06-08 18:18:48 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - a function that returns the natural square root of a number.
+ * _sqrt_recursion - a function that returns the natural 
+ * square root of a number.
  * @n: the number to be rounded
  *
  * Return: the natural square root the natural number
@@ -11,16 +12,16 @@ int _sqrt_recursion(int n)
 int hint = 0;
 if (n < 0)
 {
-return -1;
+return (-1);
 }
 else if (n == 0 || n == 1)
-return n;
+return (n);
 hint = _sqrt_recursion(n / 2);
 
 if (hint * hint == n)
-return hint;
+return (hint);
 else
 hint = (hint + n / hint) / 2;
 
-return hint;
+return (hint);
 }
