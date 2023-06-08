4a402d3383f17e6cb3bacde5aa6a0073695b6445
iAbas <abasiekemeetim20@gmail.com>
2023-06-08 11:26:21 +0100

    Determing length using recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - A function to determine the
+ * length of a string using recursion
+ * @s - Recountable string
+ *
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+    int sum = 1;
+    if (*s == '\0')
+        return 0;
+    printf("%c\n", *s);
+    int len = _strlen_recursion(s + 1) + sum;
+    return len;
+}
