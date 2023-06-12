a67ae91f4cbbbc60a0feeddc35e244625e8925bd
iAbasekeme <abasiekemeetim20@gmail.com>
2023-06-12 07:10:31 -0700

    update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - a program that prints the number
- * of arguments passed into it.
- * @argc: Argument count
- * @argv: Argument vaector
- *
- * Return: Always 0 (success)
- */
+* main - a program that prints the number
+* of arguments passed into it.
+* @argc: Argument count
+* @argv: Argument vaector
+*
+* Return: Always 0 (success)
+*/
 int main(int argc, char *argv[])
 {
-    (void)argv;
-    printf("%d", argc - 1);
-    return (0);
+(void)argv;
+printf("%d", argc - 1);
+return (0);
 }
