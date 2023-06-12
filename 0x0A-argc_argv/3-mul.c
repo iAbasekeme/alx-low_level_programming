3cf7d5f8c362f8f631afa7f7d9c3dfddc50b9915
iAbasekeme <abasiekemeetim20@gmail.com>
2023-06-12 08:56:44 -0700

    update 3-mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,26 +2,26 @@
 #include "main.h"
 
 /**
- * main - a program that prints all arguments it receives.
- * @argc: Argument count
- * @argv: argumnet vector
- *
- * Return: Always 0 (success), 1 (error)
- */
+* main - a program that prints all arguments it receives.
+* @argc: Argument count
+* @argv: argumnet vector
+*
+* Return: Always 0 (success), 1 (error)
+*/
 int main(int argc, char *argv[])
 {
-    int i = 1;
-    int mult = 0;
+int i = 1;
+int mult = 0;
 
-    if (argc <= 2)
-    {
-        printf("ERROR\n");
-        return (1);
-    }
-    for (; i < argc; i++)
-    {
-        mult = atoi(argv[1]) * atoi(argv[2]);
-    }
-    printf("mult is %d\n", mult);
-    return (0);
+if (argc <= 2)
+{
+printf("ERROR\n");
+return (1);
+}
+for (; i < argc; i++)
+{
+mult = atoi(argv[1]) * atoi(argv[2]);
+}
+printf("mult is %d\n", mult);
+return (0);
 }
