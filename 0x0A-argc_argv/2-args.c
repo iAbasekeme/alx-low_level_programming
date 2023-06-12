3e475b6f2233ca40609275fc89cb00ad8ff6ee7a
iAbasekeme <abasiekemeetim20@gmail.com>
2023-06-12 07:23:06 -0700

    update 2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,18 +2,18 @@
 #include "main.h"
 
 /**
- * main - a program that prints all arguments it receives.
- * @argc: Argument count
- * @argv: argumnet vector
- *
- * Return: Always 0 (success)
- */
+* main - a program that prints all arguments it receives.
+* @argc: Argument count
+* @argv: argumnet vector
+*
+* Return: Always 0 (success)
+*/
 int main(int argc, char *argv[])
 {
-    int i = 0;
-    for (; i < argc; i++)
-    {
-        printf("%s\n", argv[i]);
-    }
-    return (0);
+int i = 0;
+for (; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
