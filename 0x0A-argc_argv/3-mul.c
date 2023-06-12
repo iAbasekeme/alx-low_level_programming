1add2ec7a67269b85bbb47226291c6c012b98168
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-06-12 17:14:06 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,15 +13,16 @@ int main(int argc, char *argv[])
 int i = 1;
 int mult = 0;
 
-if (argc <= 2)
+if (argc != 3)
 {
 printf("ERROR\n");
 return (1);
 }
-for (; i < argc; i++)
-{
-mult = atoi(argv[1]) * atoi(argv[2]);
-}
-printf("%d\n", mult);
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2]);
+  
+int result = num1 * num2;
+
+printf("%d\n", result);
 return (0);
 }
