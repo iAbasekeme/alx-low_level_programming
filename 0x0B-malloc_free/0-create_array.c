d6f9839a9c8eef2922d74ec8cbdc274d35de3c25
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-06-13 13:58:25 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,13 @@
 */
 char *create_array(unsigned int size, char c)
 {
+char *array;
 unsigned int i = 0;
 if (size == 0)
 {
 return (NULL);
 }
-char *array = (char *)malloc(size * sizeof(char));
+array = (char *)malloc(size * sizeof(char));
 if (array == NULL)
 return (NULL);
 
