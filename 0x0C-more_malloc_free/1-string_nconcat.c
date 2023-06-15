0cfb2891bd358e5a04caa22b6e17d45b15fb01a2
iAbasekeme <abasiekemeetim20@gmail.com>
2023-06-15 05:54:02 -0700

    update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,49 +2,49 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - a function that concatenates two strings
- * @s1: first string to concatenate
- * @s2: second to be appended
- *
- * Return: Always 0 (success)
- */
+* string_nconcat - a function that concatenates two strings
+* @s1: Destination value
+* @s2: source value
+* @n: max number of char to be appended
+* Return: Always 0 (success)
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    int s1_len = 0, s2_len = 0;
-    char *ptr;
+int s1_len = 0, s2_len = 0;
+char *ptr;
 
-    if (!(s1 && s2))
-    {
-        return ("");
-    }
+if (!(s1 && s2))
+{
+return ("");
+}
 
-    while (s1[s1_len] && s2[s2_len] != '\0')
-    {
-        s1_len++;
-        s2_len++;
-    }
-    if (n >= s2[s2_len])
-    {
-        n = s2_len;
-    }
-    ptr = (char *)malloc((s1_len + n + 1) * sizeof(char));
-    if (!ptr)
-    {
-        return(NULL);
-    }
-    unsigned int i = 0, j = 0;
-    while (i < s1_len)
-    {
-        ptr[i] = s1[i];
-        i++;
-    }
-    while (j < n && s2[j] != '\0')
-    {
-        ptr[i] = s2[j];
-        i++;
-        j++;
-    }
-    ptr[i] = '\0';
+while (s1[s1_len] && s2[s2_len] != '\0')
+{
+s1_len++;
+s2_len++;
+}
+if (n >= s2[s2_len])
+{
+n = s2_len;
+}
+ptr = (char *)malloc((s1_len + n + 1) * sizeof(char));
+if (!ptr)
+{
+return (NULL);
+}
+unsigned int i = 0, j = 0;
+while (i < s1_len)
+{
+ptr[i] = s1[i];
+i++;
+}
+while (j < n && s2[j] != '\0')
+{
+ptr[i] = s2[j];
+i++;
+j++;
+}
+ptr[i] = '\0';
 
-    return (ptr);
+return (ptr);
 }
