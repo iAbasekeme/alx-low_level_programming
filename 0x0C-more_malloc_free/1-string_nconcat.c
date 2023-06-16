624887caa2dd7e314b9a8369977fecdb6e3be6a3
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-06-16 11:31:04 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,37 +9,36 @@
 * @n: max number of char to be appended
 * Return: Always 0 (success)
 */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(const char *s1, const char *s2, unsigned int n)
 {
-int s1_len = 0, s2_len = 0;
+unsigned int s1_len = 0, s2_len = 0;
 char *ptr;
 
 if (!(s1 && s2))
 {
-return ("");
+return NULL;
 }
-
-while (s1[s1_len] && s2[s2_len] != '\0')
+while (s1[s1_len] != '\0')
 {
 s1_len++;
+}
+while (s2[s2_len] != '\0' && s2_len < n)
+{
 s2_len++;
 }
-if (n >= s2[s2_len])
-{
-n = s2_len;
-}
-ptr = (char *)malloc((s1_len + n + 1) * sizeof(char));
+ptr = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
 if (!ptr)
 {
-return (NULL);
+return NULL;
 }
 unsigned int i = 0, j = 0;
+  
 while (i < s1_len)
 {
 ptr[i] = s1[i];
 i++;
 }
-while (j < n && s2[j] != '\0')
+while (j < s2_len)
 {
 ptr[i] = s2[j];
 i++;
@@ -47,5 +46,5 @@ j++;
 }
 ptr[i] = '\0';
 
-return (ptr);
+return ptr;
 }
