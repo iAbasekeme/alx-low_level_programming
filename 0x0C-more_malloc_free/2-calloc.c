0dfff4592412b07249f8450f14d5e03411e66101
iAbasekeme <abasiekemeetim20@gmail.com>
2023-06-16 17:10:38 -0700

    A function that allocates memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,43 +2,43 @@
 #include <stdlib.h>
 
 /**
- * _memset - a function that copies memory area.
- * @s: memory area to fill
- * @b: constant byte to fill
- * @n: bytes of memory area to fill
- *
- * Return: the memory area filled
- */
+* _memset - a function that copies memory area.
+* @s: memory area to fill
+* @b: constant byte to fill
+* @n: bytes of memory area to fill
+*
+* Return: the memory area filled
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
-    return (s);
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
 }
 
 /**
- * _calloc- A function that allocates memory for an array
- * @nmemb: the number of bytes to allocate
- * @size: the size of the array
- *
- * Return: Always 0 (success)
- */
+* _calloc- A function that allocates memory for an array
+* @nmemb: the number of bytes to allocate
+* @size: the size of the array
+*
+* Return: Always 0 (success)
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *ptr;
+void *ptr;
 
-    if ((nmemb == 0) && size == 0)
-    {
-        return (NULL);
-    }
-    ptr = (void *)malloc(nmemb * size);
-    if (ptr)
-    {
-        _memset(ptr, 0, nmemb * size);
-    }
-    return ptr;
+if ((nmemb == 0) && size == 0)
+{
+return (NULL);
+}
+ptr = (void *)malloc(nmemb * size);
+if (ptr)
+{
+_memset(ptr, 0, nmemb * size);
+}
+return (ptr);
 }
