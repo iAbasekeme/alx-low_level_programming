f83e3ba9cd0072a008ff4311fce55dba847e3993
iAbas <abasiekemeetim20@gmail.com>
2023-06-17 13:00:11 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,28 +12,27 @@
  */
 int main(int argc, char *argv[])
 {
-int i = 1;
-int j = 0;
-int sum = 0;
+    int i = 1;
+    int j = 0;
+    int sum = 0;
 
-for (; i < argc; i++)
-{
-if (!argv[i])
-{
-printf("0\n");
-}
-for (; argv[i][j] != '\0'; j++)
-{
-if (!isdigit(argv[i][j]))
-{
-printf("Error\n");
-return (1);
-}
-}
-sum += atoi(argv[i]);
-j = 0;
-
-}
-printf("%d\n", sum);
-return (0);
+    for (; i < argc; i++)
+    {
+        if (!argv[i])
+        {
+            printf("0\n");
+        }
+        for (; argv[i][j] != '\0'; j++)
+        {
+            if (!isdigit(argv[i][j]))
+            {
+                printf("Error\n");
+                return (1);
+            }
+        }
+        sum += atoi(argv[i]);
+        j = 0;
+    }
+    printf("%d\n", sum);
+    return (0);
 }
