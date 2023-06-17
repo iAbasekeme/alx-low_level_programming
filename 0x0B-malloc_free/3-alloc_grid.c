951220166c9077375fe0195c93266c0734e42da6
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-06-17 15:25:32 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
         return (NULL);
     for (i = 0; i < height; i++)
     {
-        grid[i] = (int *)calloc(width * sizeof(int));
+        grid[i] = (int *)calloc(width, sizeof(int));
         if (!grid[i])
         {
             for (j = 0; j < i; i++)
