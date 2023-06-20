666d657b9ccef4771312526228c9cdf590ee22c7
iAbas <abasiekemeetim20@gmail.com>
2023-06-20 16:34:18 +0100

    initializing a dog properties

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include "dog.h"
+
+/**
+ * init_dog - initializing a dog properties
+ * @d: pointer to struct Dog to initialize
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: ownwer of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    d->name = name;
+    d->age = age;
+    d->owner = owner;
+}
