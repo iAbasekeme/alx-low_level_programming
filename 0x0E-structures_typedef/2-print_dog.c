0b6f8575748e0736825a8262946399e234daba0a
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-06-20 17:46:28 +0100

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,14 +5,14 @@
  * print_dog - A function to print properties of a dog
  * @d: pointer to struct dog
  *
- * Return: Always 0 (success)
+ * Return: nothing
  */
 void print_dog(struct dog *d)
 {
 if (d != NULL)
 {
-printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-printf("Age: %.1f\n", d->age != NULL ? d->age : "(nil)");
-printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+printf("Name: %s\n", d->name != NULL ? d->name : "(nil)\n");
+printf("Age: %.1f\n", d->age);
+printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)\n");
 }
 }
