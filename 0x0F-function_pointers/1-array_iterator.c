16090a255640b5d12392adca62b198251108f277
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-06-21 20:28:04 +0100

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-if (array && size)
+if (array != NULL && size > 0 && action != NULL)
 {
 size_t i = 0;
 for (; i < size; i++)
