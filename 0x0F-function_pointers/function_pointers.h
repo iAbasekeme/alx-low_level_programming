b5bc8da57b2ba947eff6fa290c536e1f6c88d40d
iAbas <abasiekemeetim20@gmail.com>
2023-06-21 13:42:27 +0100

    Create function_pointers.h

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
