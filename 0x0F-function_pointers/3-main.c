1888a043e162555e5e1a22714f7f455c7ff74841
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-06-23 23:09:21 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,34 +4,42 @@
 #include <stdlib.h>
 
 /**
+ * main - Program Entry
  *
- */
+ * Description: Program that takes 4 arguments where 2 are
+ *              int variables plus an operator to perform
+ *              a function between the two numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0-on success
+*/
 int main(int argc, char *argv[])
 {
-    int num1, num2;
-    char *operator;
-    int (*func)(int, int);
-    int result;
+int num1, num2;
+char *operator;
+int (*func)(int, int);
+int result;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    operator= argv[2];
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+operator= argv[2];
 
-    func = get_op_func(operator);
-    if (func)
-    {
-        result = func(num1, num2);
-        printf("%d", result);
-    }
-    else
-    {
-        printf("Error\n");
-    }
-    exit(99);
+func = get_op_func(operator);
+if (func)
+{
+result = func(num1, num2);
+printf("%d", result);
+}
+else
+{
+printf("Error\n");
+}
+exit(99);
 }
