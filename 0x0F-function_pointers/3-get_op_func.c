cbb6f837cd63f0ef8c5703593cea95e5b47b1ae2
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-06-23 22:59:59 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,26 +2,31 @@
 #include "3-calc.h"
 
 /**
+ * get_op_func - get ops function pointer of type char array
+ *               that accepts two inputs of int data type
  *
- */
+ * @s: a character pointer pointing to a symbol from the program argument
+ *
+ * Return: one of the operator functions to perform calculations
+*/
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}};
-    int i = 0;
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}};
+int i = 0;
 
-    while (ops[i].op != NULL)
-    {
-        if (ops[i].op[0] == s[0])
-        {
-            return (ops[i].func);
-        }
-        i++;
-    }
-    return (NULL);
+while (ops[i].op != NULL)
+{
+if (ops[i].op[0] == s[0])
+{
+return (ops[i].func);
+}
+i++;
+}
+return (NULL);
 }
