53f8cc57d437b00dd6d90f699918859b60a2c3d6
iAbas <abasiekemeetim20@gmail.com>
2023-07-03 19:59:16 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * list_len - a function that returns the
@@ -11,34 +12,12 @@ size_t list_len(const list_t *h)
 	size_t count = 0;
 	const list_t *temp = h;
 
+	if (!temp)
+		return (0);
 	while (temp)
 	{
 		count++;
 		temp = temp->next;
 	}
 	return (count);
-}
-int main(void)
-{
-	list_t *head;
-	list_t *new;
-	list_t hello = {"World", 5, NULL};
-	size_t n;
-
-	head = &hello;
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	new->str = strdup("Hello");
-	new->len = 5;
-	new->next = head;
-	head = new;
-	n = list_len(head);
-	printf("-> %lu elements\n", n);
-	free(new->str);
-	free(new);
-	return (0);
 }
