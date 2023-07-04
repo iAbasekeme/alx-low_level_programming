3ce03543a20b103d9e1989e1a7ed6b6eec7c166a
iAbas <abasiekemeetim20@gmail.com>
2023-07-04 01:38:38 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,10 +20,41 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/**
+ * _strdup - A function that returns a pointer to a newly allocated space in
+ * memory,which contains a copy of the string given as a parameter.
+ * @str: string to be Allocated
+ *
+ * Return: Always 0 (success)
+ */
+char *_strdup(char *str)
+{
+	unsigned int i, c = 0;
+	char *string;
+
+	if (!str)
+		return (NULL);
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+	string = (char *)malloc((c + 1) * sizeof(char));
+	if (!string)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < c; i++)
+		string[i] = str[i];
+
+	string[i] = '\0';
+	return (string);
+}
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+char *_strdup(char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
