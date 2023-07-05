bfd4b3ec82e5f58b5b6f23a0caa18dd48a28ff01
iAbas <abasiekemeetim20@gmail.com>
2023-07-05 21:26:59 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,18 +8,17 @@
  *
  * Return: Number of elements in the list
  */
-
 size_t listint_len(const listint_t *h)
 {
-    size_t count = 0;
-    const listint_t *temp = h;
+	size_t count = 0;
+	const listint_t *temp = h;
 
-    if (!temp)
-        return (0);
-    while (temp != NULL)
-    {
-        count++;
-        temp = temp->n;
-    }
-    return (count);
+	if (!temp)
+		return (0);
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->n;
+	}
+	return (count);
 }
