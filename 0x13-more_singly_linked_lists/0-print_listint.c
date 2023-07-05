8b5ee462b599eed96a9a7f37616fd7574277c2df
iAbas <abasiekemeetim20@gmail.com>
2023-07-05 15:57:40 +0100

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - A function that prints all the elements
+ * of a listint_t list
+ * @h: The head property
+ *
+ * Return: All the elements in the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp = h;
+	unsigned int count = 0;
+	if (!temp)
+	{
+		return (0);
+	}
+	while (temp != NULL)
+	{
+		count++;
+	}
+	return (count);
+}
