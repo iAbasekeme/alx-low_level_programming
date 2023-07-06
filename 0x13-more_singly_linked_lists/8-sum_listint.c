a0b921a5a948060504073d2aace277da9324b6a7
iAbas <abasiekemeetim20@gmail.com>
2023-07-06 13:03:47 +0100

    Create 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * sum_listint_t - a function that returns the sum of all the data(n)
+ * @head: Pointer to the head property
+ *
+ * Return: The sum of all data(n)
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *temp = head;
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
