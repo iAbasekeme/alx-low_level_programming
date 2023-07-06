68da65bb3479e85dd785524a20ea915a31707f8a
iAbas <abasiekemeetim20@gmail.com>
2023-07-06 05:53:37 +0100

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -6,18 +6,20 @@
 /**
  * pop_listint - A function that deletes the head node in s node list
  * @head: A pointer to the head property
+ *
+ * Return: the head node's data
  */
 int pop_listint(listint_t **head)
 {
-    listint_t *temp = *head;
-    int value;
-    if (!(*head))
-    {
-        return (0);
-    }
-    *head = (*head)->next;
-    value = temp->n;
-    free(temp);
+	listint_t *temp = *head;
+	int value;
+	if (!(*head))
+	{
+		return (0);
+	}
+	*head = (*head)->next;
+	value = temp->n;
+	free(temp);
 
-    return (value);
+	return (value);
 }
