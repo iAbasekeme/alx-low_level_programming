5278f8dd0144d0c444454f319b0bab2c0757b077
iAbas <abasiekemeetim20@gmail.com>
2023-07-07 01:32:51 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * print_listint - prints all the elements of a linked list
- * @h: The head property
+ * @h: linked list of type listint_t to print
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
