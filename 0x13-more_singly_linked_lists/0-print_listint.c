ef320e08d7757a4b4dfdde0271ed890fbb4064de
iAbas <abasiekemeetim20@gmail.com>
2023-07-07 01:30:36 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * print_listint - prints all the elements of a linked list
  * @h: The head property
@@ -7,13 +6,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		count++;
+		num++;
 		h = h->next;
 	}
-	return (count);
+	return (num);
 }
