b109f387b2e2771aa744b89e2319daa5fa883886
iAbas <abasiekemeetim20@gmail.com>
2023-07-11 10:18:27 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	filedescriptor = open(filename, O_CREAT | O_WRONLY | O_EXCL | O_TRUNC, S_IRUSR, S_IWUSR);
+	filedescriptor = open(filename, O_CREAT | O_WRONLY | O_EXCL | O_TRUNC,
+						  S_IRUSR, S_IWUSR);
 	if (filedescriptor == -1)
 		return (-1);
 	file = fdopen(filedescriptor, "w");
