20cf1c51a020ba649fc4608f4942c652ab6c1f13
iAbas <abasiekemeetim20@gmail.com>
2023-07-11 11:15:42 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,13 +21,14 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		len = strlen(text_content);
-		writebytes = write(fd, text_content, len);
-
-		if (writebytes == -1 || writebytes != len)
-		{
-			return (-1);
-			close(fd);
-		}
 	}
+	writebytes = write(fd, text_content, len);
+
+	if (writebytes != len)
+	{
+		return (-1);
+		close(fd);
+	}
+
 	return (1);
 }
