80bc26123d816d965ee3c66d2673846b7ee63e41
iAbas <abasiekemeetim20@gmail.com>
2023-07-18 13:19:34 +0100

    update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,34 +13,34 @@
 
 int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int i;
-    int j;
+	int **grid;
+	int i;
+	int j;
 
-    if (width <= 0 || height <= 0)
-        return (NULL);
-    grid = (int **)malloc(height * sizeof(int *));
-    if (!grid)
-        return (NULL);
-    for (i = 0; i < height; i++)
-    {
-        grid[i] = (int *)calloc(width, sizeof(int));
-        if (!grid[i])
-        {
-            for (; j < i; j++)
-            {
-                free(grid[j]);
-            }
-            free(grid);
-            return (NULL);
-        }
-    }
-    for (i = 0; i < height; i++)
-    {
-        for (j = 0; j < width; j++)
-        {
-            grid[i][j] = 0;
-        }
-    }
-    return (grid);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = (int **)malloc(height * sizeof(int *));
+	if (!grid)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)calloc(width, sizeof(int));
+		if (!grid[i])
+		{
+			for (; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
 }
