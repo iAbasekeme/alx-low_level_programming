6cfb62d0bf485e8478c61f5ccea4ab795395e057
iAbas <abasiekemeetim20@gmail.com>
2023-08-03 23:17:09 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,6 +13,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int len = strlen(b);
 	if (!b)
 		return (0);
+	if (b[i] < '0' || b[i] > '1')
+		return (0);
 	result = 0;
 	for (i = 0; i < len; i++)
 	{
