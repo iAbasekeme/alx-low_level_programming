9b7bf6af2ec0e418c9a8c9eb42f9d1798be5c4c1
iAbas <abasiekemeetim20@gmail.com>
2023-08-03 23:32:54 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,9 +3,9 @@
 /**
  * binary_to_uint - a function that converts a
  * binary number to an unsigned int.
- * @b: number
+ * @b: binary number
  *
- * Return: unsigned int
+ * Return: new number
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -13,11 +13,12 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int len = strlen(b);
 	if (!b)
 		return (0);
-	if (b[i] < '0' || b[i] > '1')
-		return (0);
+
 	result = 0;
 	for (i = 0; i < len; i++)
 	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
 		if (b[i] == '1')
 			result += 1 << (len - 1 - i);
 	}
