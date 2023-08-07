954a8752bf7b71149838d35a0d8f582ed847ead0
iAbas <abasiekemeetim20@gmail.com>
2023-08-07 17:43:52 +0100

    Updated main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
