7b1aec96eeef8a712a4f9d89f2259e2979744903
iAbas <abasiekemeetim20@gmail.com>
2023-08-07 17:41:48 +0100

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,26 +9,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t byteWritten;
-    ssize_t len;
+	int fd;
+	ssize_t byteWritten;
+	ssize_t len;
 
-    if (!filename)
-        return (-1);
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-    if (fd == -1)
-        return (-1);
-    if (text_content != NULL)
-    {
-        len = strlen(text_content);
-        byteWritten = write(fd, text_content, len);
-        if (byteWritten == -1)
-        {
-            close(fd);
-            return -1;
-        }
-    }
-    close(fd);
-    return (1);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		byteWritten = write(fd, text_content, len);
+		if (byteWritten == -1)
+		{
+			close(fd);
+			return -1;
+		}
+	}
+	close(fd);
+	return (1);
 }
