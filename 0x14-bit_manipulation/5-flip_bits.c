a0815de6142d8a9a7253d6a64dc30a1b5507d2f8
iAbas <abasiekemeetim20@gmail.com>
2023-09-01 16:21:56 +0100

    toggling a bit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * flip_bits - a function that returns the number of bits you
+ * would need to flip to get from one number to another.
+ * @n: input
+ * @m: integer representing number of flips to make
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+    int xorResult = n ^ m;
+    int count = 0;
+
+    while (xorResult > 0)
+    {
+        if (xorResult & 1)
+            count++;
+        xorResult >>= 1;
+    }
+    return count;
+}
