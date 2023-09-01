793c3e08591fcbeef10b79fec83f6fdffa44d7e9
iAbas <abasiekemeetim20@gmail.com>
2023-09-01 02:56:23 +0100

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bit - A function to get a bit at an index
+ * @n: Number of bits
+ * @index: index
+ *
+ * Return: -1 on failure otherwise,value of bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned int) * 8)
+		return -1;
+
+	return (n >> index) & 1;
+}
