3cfac4b34c6f71090f989ea6d5061763739a5e5c
iAbas <abasiekemeetim20@gmail.com>
2023-09-01 17:10:31 +0100

    update

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -8,10 +8,11 @@
  */
 int get_endianness(void)
 {
-    unsigned int i = 1;
-    unsigned char *bytePointer = (unsigned char *)&i;
-    if (*bytePointer)
-        return (1);
-    else
-        return (0);
+	unsigned int i = 1;
+	unsigned char *bytePointer = (unsigned char *)&i;
+
+	if (*bytePointer)
+		return (1);
+	else
+		return (0);
 }
