9bf738906977cf08fa666825a8130db93dffc9fb
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-09-04 15:56:32 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_RDWR | O_TRUNC, 600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
 	if (fd == -1)
 		return (-1);
 
