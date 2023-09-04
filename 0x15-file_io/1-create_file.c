707b57c119a15544d9c7029a2ad8a7347c76db3b
iAbas <abasiekemeetim20@gmail.com>
2023-09-04 15:45:36 +0100

    a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * create_file - a function that creates a file.
+ * @filename: name of file to be created
+ * @text_content: String to be written to file
+ *
+ * Return: 1 (On success), otherwise -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int count = 0;
+	char *buffer;
+	ssize_t byteswritten;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[count] != '\0')
+	{
+		count++;
+	}
+	buffer = (char *)malloc(sizeof(char) * (count + 1));
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		strcpy(buffer, text_content);
+		byteswritten = write(fd, buffer, count);
+		if (byteswritten == -1)
+		{
+			free(buffer);
+			close(fd);
+			return (-1);
+		}
+	}
+	free(buffer);
+	close(fd);
+	return (1);
+}
