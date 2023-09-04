06ba593816e4a363930761d9745918734dd074ac
iAbas <abasiekemeetim20@gmail.com>
2023-09-04 14:40:04 +0100

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
