ec6053eaba0f32d194ec8e7e6b72b1a3bae97562
iAbas <abasiekemeetim20@gmail.com>
2023-09-04 16:06:33 +0100

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,37 +16,28 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int count = 0;
-	char *buffer;
 	ssize_t byteswritten;
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+
 	if (fd == -1)
 		return (-1);
-
 	while (text_content[count] != '\0')
 	{
 		count++;
 	}
-	buffer = (char *)malloc(sizeof(char) * (count + 1));
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (-1);
-	}
 	if (text_content != NULL)
 	{
-		strcpy(buffer, text_content);
-		byteswritten = write(fd, buffer, count);
+		byteswritten = write(fd, text_content, count);
 		if (byteswritten == -1)
 		{
-			free(buffer);
 			close(fd);
 			return (-1);
 		}
 	}
-	free(buffer);
+
 	close(fd);
 	return (1);
 }
