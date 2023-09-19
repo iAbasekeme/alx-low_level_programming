ce1440a9fdb91ecd949e98e8c2c8c6c95c49f195
iAbas <abasiekemeetim20@gmail.com>
2023-09-19 11:24:01 +0100

    fixed errs

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,8 +14,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index >= sizeof(unsigned int) * 8)
 		return (-1);
-	hold = ~(1 << index);
-	*n &= hold;
+	*n = (~(1 << index) & *n);
 
 	return (1);
 }
