74431ad08cb2e87a2b04228b01ec29b29f7e488b
iAbas <abasiekemeetim20@gmail.com>
2023-09-19 09:58:07 +0100

    fixed errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,21 +14,18 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t t;
-	char *buffer;
+	char *buf;
+	ssize_t fd;
 	ssize_t w;
+	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
-	if (!filename)
-		return (0);
 	if (fd == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters + 1);
-	t = read(fd, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, t);
-
-	free(buffer);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+	free(buf);
 	close(fd);
-	return (t);
+	return (w);
 }
