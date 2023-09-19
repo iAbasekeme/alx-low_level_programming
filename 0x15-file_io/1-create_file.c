bdffba3ce5d6078e62d265c805fda37fa63acbf9
iAbas <abasiekemeetim20@gmail.com>
2023-09-19 09:36:39 +0100

    fixed errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int count = 0;
-	int byteswritten;
+	int w;
 
 	if (!filename)
 		return (-1);
@@ -27,9 +27,9 @@ int create_file(const char *filename, char *text_content)
 			count++;
 		}
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
-	byteswritten = write(fd, text_content, count);
-	if (fd == -1 || byteswritten == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, count);
+	if (fd == -1 || w == -1)
 		return (-1);
 	close(fd);
 	return (1);
