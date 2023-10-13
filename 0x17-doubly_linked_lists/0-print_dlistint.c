8090315735e3015f0815d05f0907e5ff67f06d82
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-13 16:42:45 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+/**
+ * print_dlistint - a function that prints all the elements of a dlistint_t list.
+ * @h: variable h for accessing the n variable in the list
+ *
+ * Return: Number of nodes
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	while (h != 0)
