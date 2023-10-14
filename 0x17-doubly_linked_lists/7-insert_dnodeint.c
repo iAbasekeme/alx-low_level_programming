85d38d26bf921a5770555bc277def106bf6ddfe7
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-14 16:23:24 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,38 +1,52 @@
 #include "lists.h"
 
+/**
+ * insert_dnodeint_at_index - A fucntion that inserts a node at an index in a list
+ * @h: head of the list
+ * @idx: index of the list
+ * @n: Data from user
+ *
+ * Return: newNode
+ */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *newNode;
-    dlistint_t *current = *h;
-    unsigned int i = 0;
+	dlistint_t *newNode;
+	dlistint_t *current = *h;
+	unsigned int i = 0;
 
-    if (!*h)
-        return (NULL);
-    newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
-    if (!newNode)
-        return (NULL);
-    newNode->n = n;
+	if (!*h)
+		return (NULL);
+	newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (!newNode)
+		return (NULL);
+	newNode->n = n;
 
-    if (idx == 0)
-    {
-        return add_dnodeint(h, n);
-    }
+	if (idx == 0) // check if we at the beginning of the list
+	{
+		return add_dnodeint(h, n);
+	}
 
-    for (; current != NULL && i < idx; i++)
-    {
-        current = current->next;
-    }
+	for (; current != NULL && i < idx; i++)
+	{
+		current = current->next;
+	}
 
-    if (i < idx)
-    {
-        free(newNode);
-        return (NULL);
-    }
+	if (current == NULL) // check if we are at the end of the list
+	{
+		return (add_dnodeint_end(h, n));
+	}
 
-    newNode->prev = current;
-    newNode->next = current->next;
-    current->next = newNode;
-    newNode->next->prev = newNode;
+	if (i < idx)
+	{
+		free(newNode);
+		return (NULL);
+	}
 
-    return (newNode);
+	newNode->prev = current;
+	newNode->next = current->next;
+	current->next = newNode;
+	newNode->next->prev = newNode;
+
+	return (newNode);
 }
