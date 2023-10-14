c817d2b397064eb47babe16d2543ab061f51ee69
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-14 12:04:36 +0100

    Update 3-add_dnodeint_end.c

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - A function that adds a newnode at
+ * the end of a doublylists
+ * @head: to pointer to head of the list
+ * @n: Data from user
+ *
+ * Return: address of the newnode
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *newNode, *temp;
+
+	newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (!newNode)
+	{
+		return (NULL);
+	}
+
+	newNode->n = n;
+	newNode->next = 0;
+
+	if (!*head)
+	{
+		newNode->prev = NULL;
+		*head = newNode;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != 0)
+		{
+			temp = temp->next;
+		}
+		temp->next = newNode->prev;
+		newNode->prev = temp;
+	}
+	return (newNode);
+}
