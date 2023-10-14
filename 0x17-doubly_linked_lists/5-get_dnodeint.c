b5dbd5f982dbf000fb22845242ea4cbeee97cb00
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-14 13:42:30 +0100

    Update 5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - A function that gets the index of a list
+ * @head: head of list
+ * @index: index of node
+ *
+ * Return: index node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    unsigned int i = 0;
+    dlistint_t *current = head;
+
+    for (; current != NULL && i < index; i++)
+    {
+        current = current->next;
+    }
+    if (current == NULL)
+        return (NULL);
+    return (current);
+}
