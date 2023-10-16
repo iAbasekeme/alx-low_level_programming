05be33d9e6819156141ac0bc750bcc2f571c8f25
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-16 15:44:03 +0100

    Create 2-strchr.c

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strchr - a function that locates a character in a string.
+ * @s: the string to be scanned
+ * @c: character to be searched in the string
+ *
+ * Return: memory area replaced
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (*s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return ('\0');
+}
