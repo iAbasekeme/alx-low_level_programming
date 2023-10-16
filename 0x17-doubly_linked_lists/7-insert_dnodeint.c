f62b690078ff0081646c9b466e893b10c9f378c0
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-16 12:12:25 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,10 +41,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		free(newNode);
 		return (NULL);
 	}
-	newNode->prev = current;
-	newNode->next = current->next;
-	current->next = newNode;
-	newNode->next->prev = newNode;
-
-	return (newNode);
+	if (current == idx)
+	{
+		newNode->prev = current;
+		newNode->next = current->next;
+		current->next = newNode;
+		newNode->next->prev = newNode;
+		return (newNode);
+	}
 }
