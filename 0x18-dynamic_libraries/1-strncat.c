0ceebb865a8ad450a3fc7f2cf015f722a70fcc0b
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-16 15:44:09 +0100

    Create 1-strncat.c

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strncat - a function that concatenates two strings.
+ * @dest: destination value
+ * @src: source value
+ * @n: max number of char to be appended
+ *
+ * Return: pointer to string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0;
+	int j = 0;
+
+	while (dest[count])
+	{
+		count++;
+	}
+	while (j < n && src[j])
+	{
+		dest[count] = src[j];
+		count++;
+		j++;
+	}
+	dest[count + 1] = '\0';
+	return (dest);
+}
