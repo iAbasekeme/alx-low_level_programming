51e2fa3f1e66888ddc1799684b68afcb3d90f59b
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-16 15:44:28 +0100

    Create 9-strcpy.c

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int i = 0;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for (; i < l; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
