710acd8b58fe135860d1b4aa68b09807cbefefb1
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-06 09:33:16 +0100

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,27 +1,4 @@
 #include "hash_tables.h"
-
-/**
- * _strcmp - a function that compares two strings
- * @s1: string to be compared
- * @s2: another string to be compared
- *
- * Return: int value
- */
-int _strcmp(unsigned long int s1, unsigned long int s2)
-{
-	unsigned long int *i = 0;
-
-	while (s1[i] && s2[i] != '\0')
-	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		i++;
-	}
-	return (s1[i] - s2[i]);
-}
-
 /**
  * key_index - This function gives the index of a key
  * @key: Key of the table
