c00fdc0758f885c32fb34090d57b3110e2dae087
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-06 08:12:21 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,21 +6,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 
-    hash_table_t *ht;
-    unsigned long int i = 0;
+	hash_table_t *ht;
+	unsigned long int i = 0;
 
-    if (!size)
-        return (NULL);
+	if (!size)
+		return (NULL);
 
-    ht = (hash_table_t *)malloc(sizeof(hash_table_t));
-    if (!ht)
-        return (NULL);
-    ht->size = size;
+	ht = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+	ht->size = size;
 
-    ht->array = malloc(size * sizeof(hash_table_t *));
-    for (; i < size; i++)
-    {
-        ht->array[i] = NULL;
-    }
-    return (ht);
+	ht->array = malloc(size * sizeof(hash_table_t *));
+	for (; i < size; i++)
+	{
+		ht->array[i] = NULL;
+	}
+	return (ht);
 }
