192c4b258915f872a94a82680a5d66a9355ce7a7
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-07 18:11:55 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *kv;
 	hash_node_t *current;
-	int dup_value;
+	char *dup_value;
 	unsigned long int hash = key_index((const unsigned char *)key, ht->size);
 
 	kv = (hash_node_t *)malloc(sizeof(hash_node_t));
