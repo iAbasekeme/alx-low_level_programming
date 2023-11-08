f00c29e40cc607ee414a45a48e4df409fac76260
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-08 13:39:00 +0100

    Create 5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - A function that prints a hash table
+ * @ht: Hash table to be printed
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    hash_node_t *cur;
+    unsigned long int s = 0;
+    int first;
+
+    if (ht == NULL)
+    {
+        printf("\n");
+    }
+    first = 1;
+    printf("{");
+    for (; s < ht->size; s++)
+    {
+        cur = ht->array[s];
+        while (cur != NULL)
+        {
+            if (!first)
+            {
+                printf(", ");
+            }
+            printf("'%s': '%s'", cur->key, cur->value);
+            first = 0;
+            cur = cur->next;
+        }
+    }
+    printf("}\n");
+}
