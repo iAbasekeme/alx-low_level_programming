cd0a4667de281bbd267c833ecca6706f9e915221
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-08 18:27:55 +0100

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -5,6 +5,22 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i = 0;
+	hash_node_t *current, *temp;
+
+	for (; i < ht->size; i++)
+	{
+		current = ht->array[i];
+
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
 	free(ht->array);
 	free(ht);
 }
