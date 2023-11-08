6163fcc80cb450067c3e776997b9c1e8fb359f5e
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-08 13:43:27 +0100

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -5,29 +5,29 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *cur;
-    unsigned long int s = 0;
-    int first;
+	hash_node_t *cur;
+	unsigned long int s = 0;
+	int first;
 
-    if (ht == NULL)
-    {
-        printf("\n");
-    }
-    first = 1;
-    printf("{");
-    for (; s < ht->size; s++)
-    {
-        cur = ht->array[s];
-        while (cur != NULL)
-        {
-            if (!first)
-            {
-                printf(", ");
-            }
-            printf("'%s': '%s'", cur->key, cur->value);
-            first = 0;
-            cur = cur->next;
-        }
-    }
-    printf("}\n");
+	if (ht == NULL)
+	{
+		printf("\n");
+	}
+	first = 1;
+	printf("{");
+	for (; s < ht->size; s++)
+	{
+		cur = ht->array[s];
+		while (cur != NULL)
+		{
+			if (!first)
+			{
+				printf(", ");
+			}
+			printf("'%s': '%s'", cur->key, cur->value);
+			first = 0;
+			cur = cur->next;
+		}
+	}
+	printf("}\n");
 }
