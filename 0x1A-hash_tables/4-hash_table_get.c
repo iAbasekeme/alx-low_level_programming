5ab4fac99c8ef0b738c8994db92980394b0e19d2
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-08 12:34:26 +0100

    Create 4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - A function that gets a values associated with a key
+ * @ht: The table of array to search through
+ * @key: The key to be retrieved
+ *
+ * Return: The value associated with the element,
+ * or NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *curr;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+	{
+		return NULL;
+	}
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	curr = ht->array[index];
+
+	while (curr != NULL)
+	{
+		if (strcmp(curr->key, key) == 0)
+		{
+			return (curr->value);
+		}
+		curr = curr->next;
+	}
+	return (NULL);
+}
