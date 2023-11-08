967372a94eaf6ffad1c4a8b7e75c922e2f6b8062
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-08 12:35:12 +0100

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,7 +14,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (ht == NULL || key == NULL || *key == '\0')
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	index = key_index((const unsigned char *)key, ht->size);
