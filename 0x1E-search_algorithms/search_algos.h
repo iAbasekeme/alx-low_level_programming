6ffd1fca87eec358539509b8a7197156f02383cb
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-06 23:47:51 +0100

    Create search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef __SEARCH_ALGOS_H
+#define __SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+#endif
