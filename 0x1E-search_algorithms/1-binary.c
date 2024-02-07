237c8c50454fa70d7c3a0182f66121fb66aa6b84
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-07 16:47:52 +0100

    fixed wrong indentation on file

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,15 +8,15 @@
  */
 void print_array(int *array, size_t L, size_t R)
 {
-    size_t i;
+	size_t i;
 
-    for (i = L; i <= R; i++)
-    {
-        if (i != R)
-            printf("%d, ", array[i]);
-        else
-            printf("%d\n", array[i]);
-    }
+	for (i = L; i <= R; i++)
+	{
+		if (i != R)
+			printf("%d, ", array[i]);
+		else
+			printf("%d\n", array[i]);
+	}
 }
 
 /**
@@ -30,32 +30,32 @@ void print_array(int *array, size_t L, size_t R)
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t low;
-    size_t high;
-    size_t middle;
+	size_t low;
+	size_t high;
+	size_t middle;
 
-    low = 0;
-    high = size - 1;
+	low = 0;
+	high = size - 1;
 
-    if (array == NULL || size <= 0)
-        return (-1);
+	if (array == NULL || size <= 0)
+		return (-1);
 
-    while (low <= high)
-    {
-        printf("Searching in array: ");
-        print_array(array, low, high);
-        middle = low + (high - low) / 2;
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+		print_array(array, low, high);
+		middle = low + (high - low) / 2;
 
-        if (array[middle] == value)
-        {
-            return (value);
-        }
-        if (array[middle] < value)
-        {
-            low = middle + 1;
-        }
-        else
-            high = middle - 1;
-    }
-    return (-1);
+		if (array[middle] == value)
+		{
+			return (value);
+		}
+		if (array[middle] < value)
+		{
+			low = middle + 1;
+		}
+		else
+			high = middle - 1;
+	}
+	return (-1);
 }
