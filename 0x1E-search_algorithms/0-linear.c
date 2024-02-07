258db4d87df15dc1cd9ddd0e8bbd908acca1ac26
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-07 16:42:32 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,18 +11,18 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-        size_t i;
+    size_t i;
 
-        if (array != NULL)
+    if (array != NULL)
+    {
+        for (i = 0; i < size; i++)
         {
-                for (i = 0; i < size; i++)
-                {
-                        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-                        if (array[i] == value)
-                        {
-                                return (i);
-                        }
-                }
+            printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+            if (array[i] == value)
+            {
+                return (i);
+            }
         }
-        return (-1);
+    }
+    return (-1);
 }
